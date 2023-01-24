713b92880d665040fc6c402998e6e7b2d537f9b1
MarkoM10 <58051779+MarkoM10@users.noreply.github.com>
2023-01-24 14:08:13 +0100

    Third commit
    Building CountryInfo page and fetching API for specific country. Building SearchFilter component. Designing previously mentioned.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import './App.css';
 import Home from './Pages/Home';
 import Navbar from './Components/Navbar';
+import CountryInfo from './Pages/CountryInfo';
+import {BrowserRouter, BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { useState } from 'react';
+import Context from './Components/Context';
+import SearchFilter from './Components/SearchFilter';
 
 function App() {
 
 
-
-
-
-
-
-
-
-
+  const[searchTerm,setSearchTerm] = useState("");
 
   return (
     <div className='main-wrapper'>
+      <BrowserRouter>
+      <Context.Provider value={{searchTerm,setSearchTerm}}>
         <Navbar/>
-        <Home/>
+        <SearchFilter/>
+            <Routes>
+              <Route path='/' index element={<Home/>}/>
+              <Route path=':name' element={<CountryInfo/>}/>
+            </Routes>
+          </Context.Provider>
+      </BrowserRouter>
     </div>
   );
 }
-
 export default App;
